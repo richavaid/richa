f0e44c75d6bbd0ca20a1571ed02d2b2e4b048898
richavaid <74708466+richavaid@users.noreply.github.com>
2020-11-30 16:56:12 +0530

    edit hero section and install deps

--- src/mock/data.js
+++ src/mock/data.js
@@ -9,57 +9,66 @@ export const headData = {
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: 'Hey,there!! We are',
+  name: 'Kanooni Guide',
+  subtitle: 'One stop for all the legal services and solutions',
+  cta: 'Know more',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  img: 'about.png',
+  paragraphOne: 'Kanooni Guide was started 5 years back with the sole aim of providing legal services in a very efficient way. We help by simplifying legal business-related matters and commited in helping individuals, startups, small business owners.',
+  paragraphTwo: 'Our mission is to offer affordable, quick legal services to our clients. In these 5 years we have helped many by offerimg broad spectrum of services which includes Co-operates, Mergers and Acqusition, Financial services, Disputes, Capital market, Tax and Reasl Estate..',
+  paragraphThree: 'With us you will get th best legal solution which will help you to grow in fast paced world.',
+  
 };
 
-// PROJECTS DATA
+// SERVICES DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
+    img: 'bank_.png',
+    title: 'Banking and Finance',
+    info: 'Banking and finance is one of our core and highly recognised areas of expertise.',
+    info2: 'Our full suite of services range from project finance, trade and export finance, working capital, acquisition finance, corporate finance, syndicated lending, structured finance, debt capital markets, securitisation, regulatory advice, and product documentation and standardisation.',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
+    img: 'insurance.png',
+    title: 'Insurance',
+    info: 'Kanooni Guide has a market leading insurance/reinsurance sector practice on both the contentious and non-contentious side..',
+    info2: 'On the non-contentious front, we provide assistance with transactional work whereas in relation to contentious work, the Firm has the ability to handle and advise on claims  including Financial & Specialty Lines (Tech E&O; Professional Indemnity; Directors & Officers; Media Liability; Cyber-risks ',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
+    img: 'dispute',
+    title: 'Dispute Resolution - Arbitration & Litigation',
+    info: 'we work in almost every area of dispute resolution',
+    info2: 'Our dispute resolution team regularly advise clients on a variety of contentious matters involving commercial contracts, civil disputes, shareholders’ disputes, enforcement of security, recovery of debts, corporate and securities law, competition law, labour and employment matters, real estate issues and economic offences.',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+  
+  },
+  {
+    id: nanoid(),
+    img: 'corporate.png',
+    title: 'Corporate and Commercial',
+    info: ' Kannoni Guide key practice areas is corporate and commercial, which extends to various sectors of the economy and wide array of business and transactional matters.',
+    info2: 'The Firm’s experience and expertise in providing advice on corporate and commercial contracts and joint ventures, inbound and outbound investment, regulatory and compliance, corporate governance, operational and structuring matters across a broad range of sectors is well recognised..',
+    url: '',
+  
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
+  cta: 'Would you wnat us to work for you? Awsome!!',
+  btn: 'Lets Talk',
+  email: 'mailto:kanooniguide@gmail,com',
 };
 
 // FOOTER DATA
@@ -68,22 +77,18 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'twitter',
-      url: '',
+      url: 'https://twitter.com/KanooniG',
     },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
+    
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/shivam-tyagi-38b266200/',
     },
     {
       id: nanoid(),
-      name: 'github',
-      url: '',
+      name: 'instagram',
+      url: 'https://www.instagram.com/kanooniguide/',
     },
   ],
 };
